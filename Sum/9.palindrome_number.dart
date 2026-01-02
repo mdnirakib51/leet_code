6e68a61bd48a9ad5dfc144ddb7d347763182961f
Md. Nazrul Islam Rakib <md.rakib3248@gmail.com>
2026-01-02 16:09:45 +0600

    Palindrome Number

--- Sum/9.palindrome_number.dart
+++ Sum/9.palindrome_number.dart
@@ -1,12 +1,12 @@
+
 class Solution{
-  bool isPalindrome(int x){
+  bool? isPalindrome(int x){
     if(x < 0){
       return false;
     }
 
     int original = x;
     int rev = 0;
-
     while(x != 0){
       int digit = x % 10;
       x ~/= 10;
@@ -14,9 +14,29 @@ class Solution{
       rev = rev * 10 + digit;
     }
 
-    return original == rev;
+    return rev == original;
   }
 }
+
+// class Solution{
+//   bool isPalindrome(int x){
+//     if(x < 0){
+//       return false;
+//     }
+
+//     int original = x;
+//     int rev = 0;
+
+//     while(x != 0){
+//       int digit = x % 10;
+//       x ~/= 10;
+
+//       rev = rev * 10 + digit;
+//     }
+
+//     return original == rev;
+//   }
+// }
 
 void main() {
   Solution s = Solution();
