334f56a945a87e32f0a33784af3abaf122b688db
Md. Nazrul Islam Rakib <md.rakib3248@gmail.com>
2026-01-14 19:24:50 +0600

    Remove Duplicates From Sorted Array

--- Sum/26.remove_duplicates_from_sorted_arrya.dart
+++ Sum/26.remove_duplicates_from_sorted_arrya.dart
@@ -1,5 +1,6 @@
-class Solution {
-  int removeDuplicates(List<int> nums) {
+
+class Solution{
+  int removeDuplicates(List<int> nums){
     if(nums.isEmpty) return 0;
 
     int i = 0;
@@ -13,6 +14,22 @@ class Solution {
     return i + 1;
   }
 }
+
+// class Solution {
+//   int removeDuplicates(List<int> nums) {
+//     if(nums.isEmpty) return 0;
+
+//     int i = 0;
+//     for(int j = 0; j < nums.length; j++){
+//       if(nums[j] != nums[i]){
+//         i++;
+//         nums[i] = nums[j];
+//       }
+//     }
+
+//     return i + 1;
+//   }
+// }
 
 void main(){
   final s = Solution();
--- Sum/Python/11.container_with_most_water.py
+++ Sum/Python/11.container_with_most_water.py
@@ -1,5 +1,5 @@
-from typing import List
 
+from typing import List
 class Solution:
     def maxArea(self, height: List[int])-> int:
         left = 0
@@ -19,8 +19,31 @@ class Solution:
                 left += 1
             else:
                 right -= 1
-
         return maxArea
+
+# from typing import List
+
+# class Solution:
+#     def maxArea(self, height: List[int])-> int:
+#         left = 0
+#         right = len(height) - 1
+#         maxArea = 0
+
+#         while left < right:
+#             h = min(height[left], height[right])
+#             w = right - left
+
+#             area = h * w
+
+#             if area > maxArea:
+#                 maxArea = area
+
+#             if height[left] < height[right]:
+#                 left += 1
+#             else:
+#                 right -= 1
+
+#         return maxArea
 
 # ====@ Date: 13-01-2026 @====
 # class Solution:
